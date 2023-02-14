5b3b362db71119856f5ba9c55890a1a47ce577ec
lectureazi <lectureazi@gmail.com>
2023-02-14 09:18:36 +0900

    init

---
+++ a_jpa/src/com/mc/bookmanager/Run.java
@@ -0,0 +1,11 @@
+package com.mc.bookmanager;
+
+import com.mc.bookmanager.jpa.EntityTemplate;
+
+public class Run {
+	
+	 public static void main(String[] args) {
+		EntityTemplate.getEntityManager();
+	}
+
+}
---
+++ a_jpa/src/com/mc/bookmanager/rent/RentRepository.java
@@ -0,0 +1,5 @@
+package com.mc.bookmanager.rent;
+
+public class RentRepository {
+
+}
